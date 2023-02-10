712d6a5102286fa534cbf0a45becde74c07599ba
from1to2 <ngcd12345@gmail.com>
2023-02-10 17:43:23 +0900

    style: em 태그 추가

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import './Detail.css';
 
 const Detail = () => {
     const detailItem = useSelector(state=> state.product);
@@ -9,11 +10,18 @@ const Detail = () => {
     console.log(params.id);
     return (
         <div>
-            <h2>Food Description</h2>
-            <h3>{detailItem.map((item)=>{
-                 return  `${item.id === Number(params.id) ?  item.food_description : ''}`
-            })}</h3>
-            <h3>This food's ID is {params.id}</h3>
+            <h2>Food Description: <em>{detailItem.map((item)=>{
+                 return  `${item.id === Number(params.id) ?  item.product_name : ''}`
+            })}</em></h2>
+            <br></br>
+            <br></br>
+           
+                <p >{detailItem.map((item)=>{
+                    return  `${item.id === Number(params.id) ?  item.food_description : ''}`
+                })}</p>
+           
+            
+            
         </div>
     );
 };
