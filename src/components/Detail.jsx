9933617d52ad39c5b98075454072028eef5b2b78
from1to2 <ngcd12345@gmail.com>
2023-02-10 17:27:49 +0900

    feat: 음식 상세설명 추가

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -6,13 +6,12 @@ const Detail = () => {
     const detailItem = useSelector(state=> state.product);
     let params = useParams();
     console.log(detailItem);
+    console.log(params.id);
     return (
         <div>
             <h2>Food Description</h2>
-            <h3>{detailItem.map((item,idx)=>{
-                return(
-                    item.idx+1 === params.id ? item.food_description : null
-                )
+            <h3>{detailItem.map((item)=>{
+                 return  `${item.id === Number(params.id) ?  item.food_description : ''}`
             })}</h3>
             <h3>This food's ID is {params.id}</h3>
         </div>
